0b4e97c6effd4fd6f690cef0fc096830dfe0a2bc
unknown <office.webniac@gmail.com>
2022-09-13 08:07:46 +0300

    carousel 100%

--- components/sixth-section/text.js
+++ components/sixth-section/text.js
@@ -6,8 +6,9 @@ import {
   useSpring,
   Image,
   useMeasure,
+  useEmblaCarousel,
 } from "../../lib/external-components";
-import { useSize } from "../../hooks/resize-observer";
+
 const data = [
   {
     id: "first",
@@ -47,7 +48,11 @@ const data = [
     id: "ninth",
   },
 ];
+// .embla__slide {
+//     flex: 0 0 100%;
+//   }
 const Test = () => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
   const optimizedData = data.reduce((resultArr, item, index) => {
     const chunkIndex = Math.floor(index / 4);
     if (!resultArr[chunkIndex]) {
@@ -56,33 +61,6 @@ const Test = () => {
     resultArr[chunkIndex].push(item);
     return resultArr;
   }, []);
-  const sliderRef = useRef();
-  const slidesRef = useRef();
-  const sliderSize = useSize(sliderRef);
-  const slidesSize = useSize(slidesRef);
-
-  const [sliderWidth, setSliderWidth] = useState();
-  const [slidesWidth, setSlidesWidth] = useState();
-
-  useEffect(() => {
-    console.log(sliderWidth);
-  }, [sliderWidth]);
-  useEffect(() => {
-    console.log(slidesWidth);
-  }, [slidesWidth]);
-
-  useEffect(() => {
-    if (sliderSize) {
-      setSliderWidth(sliderSize);
-    }
-  }, [sliderSize]);
-
-  useEffect(() => {
-    if (slidesSize) {
-      setSlidesWidth(slidesSize);
-    }
-  }, [slidesSize]);
-
   return (
     <div className="w-[90vw] mt-16 flex flex-col gap-16 items-center">
       <div className="flex flex-row justify-center">
@@ -92,28 +70,14 @@ const Test = () => {
       </div>
       {/* carousel */}
       <div
-        ref={sliderRef}
-        className="w-[90vw] overflow-x-hidden hover:cursor-grab active:cursor-grabbing active:scale-[.95] border-2 border-green-500"
+        ref={emblaRef}
+        className="w-full overflow-x-hidden hover:cursor-grab active:cursor-grabbing active:scale-[.95]"
       >
-        <motion.ul
-          className="flex list-none"
-          ref={slidesRef}
-          drag="x"
-          dragConstraints={{
-            left: -(
-              slidesWidth?.width +
-              slidesWidth?.right -
-              sliderWidth?.width
-            ),
-            right: 0,
-          }}
-          dragElastic={0.2}
-          dragTransition={{ bounceDamping: 18 }}
-        >
+        <motion.ul className="flex list-none">
           {optimizedData.map((d, i) => {
             return (
               <li key={i} className="">
-                <motion.div className="grid gap-2 w-[90vw] flex-shrink-0 h-[40vh] sm:h-[60vh] md:h-[70vh] grid-cols-[1fr_1fr_1fr_1fr] grid-rows-[2fr_1fr_1fr_1fr] p-1">
+                <motion.div className="grid gap-2 w-[90vw] flex-shrink-0 h-[30vh] sm:h-[60vh] md:h-[70vh] grid-cols-[1fr_1fr_1fr_1fr] grid-rows-[2fr_1fr_1fr_1fr] p-1">
                   <div className="relative col-start-1 col-end-3 row-start-1 row-end-5">
                     {d.length >= 0 && (
                       <Image
