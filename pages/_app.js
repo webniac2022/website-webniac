513afd4fc80d6f8617d0449cd6ca476006ef8c95
unknown <office.webniac@gmail.com>
2022-10-03 12:00:53 +0300

    analythics

--- pages/_app.js
+++ pages/_app.js
@@ -4,22 +4,37 @@ import Layout from "../components/layout/layout";
 import { ThemeProvider } from "next-themes";
 import { ApolloProvider } from "@apollo/client/react";
 import client from "../lib/apollo";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider
-        attribute="class"
-        themes={["dark", "light"]}
-        enableSystem={false}
-      >
-        <AppWrapper>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </AppWrapper>
-      </ThemeProvider>
-    </ApolloProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTHICS_MEASUREMENT_CODE}`}
+      />
+      <Script
+        strategy="lazyOnload"
+        src={`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', ${process.env.GOOGLE_ANALYTHICS_MEASUREMENT_CODE});`}
+      />
+      <ApolloProvider client={client}>
+        <ThemeProvider
+          attribute="class"
+          themes={["dark", "light"]}
+          enableSystem={false}
+        >
+          <AppWrapper>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </AppWrapper>
+        </ThemeProvider>
+      </ApolloProvider>
+    </>
   );
 }
 
