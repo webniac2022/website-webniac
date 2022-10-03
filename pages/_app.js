8fc3ebc8e4839365d301a69f926ae67a1ff75f9a
unknown <office.webniac@gmail.com>
2022-10-03 13:29:27 +0300

    tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -9,18 +9,6 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){window.dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-            `}
-      </Script>
       <ApolloProvider client={client}>
         <ThemeProvider
           attribute="class"
