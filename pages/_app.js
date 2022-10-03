41241ff911f5c58ea3330628886a683105e8e2d5
unknown <office.webniac@gmail.com>
2022-10-03 13:43:22 +0300

    analithics

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,18 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      ></Script>
+      <Script id="google-analythics-script" strategy="afterInteractive">{`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+      page_path: window.location.pathname,
+      });
+      `}</Script>
       <ApolloProvider client={client}>
         <ThemeProvider
           attribute="class"
